721fe4db54160eab67977388269118a61529ff6e
montasir <mutawirsoft@mail.ru>
2018-12-30 13:16:03 +0300

    Semantic UI IntroductionSection 3, Lecture 20

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import { Button } from 'semantic-ui-react';
 
 class App extends Component {
   render() {
     return (
       <div>
         <h1>Renewable Energy Events</h1>
+      <button className="ui icon button">
+        <i className="smile icon"></i>
+        CSS button
+      </button>
+      
+      <Button icon="smile" content="React Button"/>
       </div>
     );
   }
